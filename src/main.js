d1812cabd7da338c90900b379e461b62aa8304ed
duyettwyler2k <duyettwyller@gmail.com>
2021-07-15 14:05:42 +0700

    'asd'

---
+++ src/main.js
@@ -0,0 +1,6 @@
+var width;
+var height;
+width = 3;
+height = 10;
+var area = width * height;
+console.log("Dien tich hinh chu nhat la:" + area);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+let width:number;
+let height:number;
+width=3;
+height=10;
+let area:number=width*height;
+console.log(`Dien tich hinh chu nhat la:${area}git `)
